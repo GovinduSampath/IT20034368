0c5512b7e7535cf8d034476c192b8ea276b2cee9
Govindu Sampath <88320409+GovinduSampath@users.noreply.github.com>
2022-05-15 20:15:08 +0530

    init commit

---
+++ WebContent/Components/items.js
@@ -0,0 +1,151 @@
+$(document).ready(function() 
+{ 
+if ($("#alertSuccess").text().trim() == "") 
+ { 
+ $("#alertSuccess").hide(); 
+ } 
+ $("#alertError").hide(); 
+}); 
+// SAVE 
+function onItemSaveComplete(response, status) 
+{ 
+if (status == "success") 
+ { 
+ var resultSet = JSON.parse(response); 
+ if (resultSet.status.trim() == "success") 
+ { 
+ $("#alertSuccess").text("Successfully saved."); 
+ $("#alertSuccess").show(); 
+ $("#divItemsGrid").html(resultSet.data); 
+ } else if (resultSet.status.trim() == "error") 
+ { 
+ $("#alertError").text(resultSet.data); 
+ $("#alertError").show(); 
+ } 
+ } else if (status == "error") 
+ { 
+ $("#alertError").text("Error while saving."); 
+ $("#alertError").show(); 
+ } else
+ { 
+ $("#alertError").text("Unknown error while saving.."); 
+ $("#alertError").show(); 
+ } 
+ 
+ $("#hidItemIDSave").val(""); 
+ $("#formItem")[0].reset(); 
+}
+
+$(document).on("click", "#btnSave", function(event) 
+{ 
+// Clear alerts---------------------
+ $("#alertSuccess").text(""); 
+ $("#alertSuccess").hide(); 
+ $("#alertError").text(""); 
+ $("#alertError").hide(); 
+// Form validation-------------------
+var status = validateItemForm(); 
+if (status != true) 
+ { 
+ $("#alertError").text(status); 
+ $("#alertError").show(); 
+ return; 
+ } 
+// If valid------------------------
+var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT"; 
+ $.ajax( 
+ { 
+ url : "paymentAPI", 
+ type : type, 
+ data : $("#formItem").serialize(), 
+ dataType : "text", 
+ complete : function(response, status) 
+ { 
+ onItemSaveComplete(response.responseText, status); 
+ } 
+ }); 
+}); 
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function(event) 
+{ 
+ $("#hidItemIDSave").val($(this).data("itemid")); 
+
+ $("#customerName").val($(this).closest("tr").find('td:eq(0)').text()); 
+ $("#email").val($(this).closest("tr").find('td:eq(1)').text()); 
+ $("#contactNo").val($(this).closest("tr").find('td:eq(2)').text()); 
+ $("#amount").val($(this).closest("tr").find('td:eq(3)').text()); 
+ $("#paymentDate").val($(this).closest("tr").find('td:eq(4)').text()); 
+}); 
+
+$(document).on("click", ".btnRemove", function(event) 
+{ 
+ $.ajax( 
+ { 
+ url : "paymentAPI", 
+ type : "DELETE", 
+ data : "paymentID=" + $(this).data("itemid"),
+ dataType : "text", 
+ complete : function(response, status) 
+ { 
+ onItemDeleteComplete(response.responseText, status); 
+ } 
+ }); 
+});
+function onItemDeleteComplete(response, status) 
+{ 
+if (status == "success") 
+ { 
+ var resultSet = JSON.parse(response); 
+ if (resultSet.status.trim() == "success") 
+ { 
+ $("#alertSuccess").text("Successfully deleted."); 
+ $("#alertSuccess").show(); 
+ $("#divItemsGrid").html(resultSet.data); 
+ } else if (resultSet.status.trim() == "error") 
+ { 
+ $("#alertError").text(resultSet.data); 
+ $("#alertError").show(); 
+ } 
+ } else if (status == "error") 
+ { 
+ $("#alertError").text("Error while deleting."); 
+ $("#alertError").show(); 
+ } else
+ { 
+ $("#alertError").text("Unknown error while deleting.."); 
+ $("#alertError").show(); 
+ } 
+}
+
+
+// CLIENT-MODEL================================================================
+function validateItemForm() 
+{ 
+// CODE
+if ($("#customerName").val().trim() == "") 
+ { 
+ return "Insert Name."; 
+ } 
+// NAME
+if ($("#email").val().trim() == "") 
+ { 
+ return "Insert Email."; 
+ } 
+ 
+// PRICE-------------------------------
+if ($("#contactNo").val().trim() == "") 
+ { 
+ return "Insert Contact No."; 
+ } 
+ 
+// DESCRIPTION------------------------
+if ($("#amount").val().trim() == "") 
+ { 
+ return "Insert amount."; 
+ } 
+if ($("#paymentDate").val().trim() == "") 
+ { 
+ return "Insert paymentDate."; 
+ } 
+return true; 
+}
